4ae05ef90f5a59aa89eed86b3cdb9de7a60821cb
samliu <samliuforwork@gmail.com>
2021-02-25 23:30:52 +0800

    crawler

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,10 +1,13 @@
 class StocksController < ApplicationController
+  require 'rubygems'
+  require 'nokogiri'
+  require "rss"
+  require "open-uri"
+
   def watchlist
   end
 
   def news
-    require "rss"
-    require "open-uri"
     rss_results = []
     @rss = RSS::Parser.parse(open('http://feeds.feedburner.com/CoinDesk?format=xml').read, false).items[0..5]
     @dataroma = RSS::Parser.parse(open('http://feeds.feedburner.com/dataroma').read, false).items[0..5]
@@ -13,5 +16,11 @@ class StocksController < ApplicationController
     #   rss_results.push(result)
     # end
     #   return rss_results
+  end
+
+  def crawler
+    page_url = "https://dataroma.com/m/managers.php"
+    @page = Nokogiri::HTML(open(page_url))
+    @number_investor = @page.css("tbody").css("tr td.man").count
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resource :stocks, only: [] do
     get "watchlist"
     get "news"
+    get "crawler"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
